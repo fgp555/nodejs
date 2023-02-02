ef1ee3a084cfc1cc6bb10a2c609b464a44557f70
Frank GP <fgp555@gmail.com>
2023-02-02 06:52:38 -0500

    04

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const http = require("http")
 http.createServer(function (request, response) {
     response.write("hello world")
     response.end()
-}).listen(3000)
+}).listen(80)
 
 console.log("servidor escuchando en el puerto localhost:3000")
